647d9680861689f276c0bee66f80d6af0535f6b9
Eugenio <eugenioclrc@gmail.com>
2024-04-21 13:06:27 -0300

    add extra comments

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -7,6 +7,8 @@ contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
+        // Used by default when deploying with create2, https://github.com/Arachnid/deterministic-deployment-proxy.
+        require(CREATE2_FACTORY.code.length > 0, "CREATE2DEPLOYER NOT DEPLOYED!");
         vm.broadcast();
     }
 }
